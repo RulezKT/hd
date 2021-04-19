9f29b511c46deed3982d24d33a2da7997ee4401f
RulezKT <rudakov.de@gmail.com>
2021-04-19 09:13:45 +0200

    added CalcHexLineColorToneBase func

--- hd.go
+++ hd.go
@@ -7,6 +7,233 @@ import (
 	"github.com/RulezKT/structs"
 )
 
+
+const (
+	// размер одной линии в десятичных градусах
+	oneLineInDec float64 = 0.9375
+
+	// размер одного цвета в десятичных градусах
+	oneColorInDec float64 = 0.15625
+
+	// размер одного тона в десятичных градусах
+	oneToneInDec float64 = 0.026041666666666668
+
+	// размер одной базы в десятичных градусах
+	oneBaseInDec float64 = 0.005208333333333334
+)
+
+//гексаграммы с  диапазонами градусов на космограмме, первое значение включая, второе не включая
+type HexRangeRAD struct {
+	startDegree float64
+	endDegree   float64
+}
+
+func CalcHexLineColorToneBase(longitude float64) structs.HdStructure {
+
+	var hexSortByDeg = map[int]HexRangeRAD{
+
+		// from 3.875 to 9.49
+		17: {3.875, 9.5},
+
+		21: {9.5, 15.125},
+
+		51: {15.125, 20.75},
+
+		42: {20.75, 26.375},
+
+		3: {26.375, 32.0},
+
+		27: {32.0, 37.625},
+
+		24: {37.625, 43.25},
+
+		2: {43.25, 48.875},
+
+		23: {48.875, 54.5},
+
+		8: {54.5, 60.125},
+
+		20: {60.125, 65.75},
+
+		16: {65.75, 71.375},
+
+		35: {71.375, 77.0},
+
+		45: {77.0, 82.625},
+
+		12: {82.625, 88.255},
+
+		15: {88.25, 93.875},
+
+		52: {93.875, 99.5},
+
+		39: {99.5, 105.125},
+
+		53: {105.125, 110.75},
+
+		62: {110.75, 116.375},
+
+		56: {116.375, 122.0},
+
+		31: {122.0, 127.625},
+
+		33: {127.625, 133.25},
+
+		7: {133.25, 138.875},
+
+		4: {138.875, 144.5},
+
+		29: {144.5, 150.125},
+
+		59: {150.125, 155.75},
+
+		40: {155.75, 161.375},
+
+		64: {161.375, 167.0},
+
+		47: {167.0, 172.625},
+
+		6: {172.625, 178.25},
+
+		46: {178.25, 183.875},
+
+		18: {183.875, 189.5},
+
+		48: {189.5, 195.125},
+
+		57: {195.125, 200.75},
+
+		32: {200.75, 206.375},
+
+		50: {206.375, 212.0},
+
+		28: {212.0, 217.625},
+
+		44: {217.625, 223.25},
+
+		1: {223.25, 228.875},
+
+		43: {228.875, 234.5},
+
+		14: {234.5, 240.125},
+
+		34: {240.125, 245.75},
+
+		9: {245.75, 251.375},
+
+		5: {251.375, 257.0},
+
+		26: {257.0, 262.625},
+
+		11: {262.625, 268.25},
+
+		10: {268.25, 273.875},
+
+		58: {273.875, 279.5},
+
+		38: {279.5, 285.125},
+
+		54: {285.125, 290.75},
+
+		61: {290.75, 296.375},
+
+		60: {296.375, 302.0},
+
+		41: {302.0, 307.625},
+
+		19: {307.625, 313.25},
+
+		13: {313.25, 318.875},
+
+		49: {318.875, 324.5},
+
+		30: {324.5, 330.125},
+
+		55: {330.125, 335.75},
+
+		37: {335.75, 341.375},
+
+		63: {341.375, 347.0},
+
+		22: {347.0, 352.625},
+
+		36: {352.625, 358.25},
+
+		25: {358.25, 3.875},
+	}
+
+	var hex int
+
+	var line float64
+
+	var color float64
+
+	var tone float64
+
+	var base float64
+
+	var number_of_passed_degrees float64
+
+	longitude *= mathfn.RAD_TO_DEG
+
+	longitude = convert_to_0_360_DEG(longitude)
+	// console.log(`longitude = ${longitude}`);
+
+	for key, entry := range hexSortByDeg {
+
+		//25 - последняя гексаграмма и там начальное значение больше конечного, круг замыкается
+		if (longitude >= entry.startDegree) &&
+			(longitude < entry.endDegree || key == 25) {
+
+			hex = key
+			//System.out.println("hex = " + hex + "===" + longitude);
+			number_of_passed_degrees = longitude - entry.startDegree
+
+			line = number_of_passed_degrees / oneLineInDec
+
+			color = (number_of_passed_degrees - float64(int(line))*oneLineInDec) / oneColorInDec
+			// console.log(`Цвет   = ${math.ceil(color)}, цвет завершена на ${(color - math.trunc(color))*100}  процентов`);
+
+			tone = (number_of_passed_degrees - float64(int(line))*oneLineInDec - float64(int(color))*oneColorInDec) / oneToneInDec
+			// console.log(`Тон  =  ${math.ceil(tone)},тон завершен на ${(tone - math.trunc(tone))*100} процентов`);
+
+			base = (number_of_passed_degrees - float64(int(line))*oneLineInDec - float64(int(color))*oneColorInDec - float64(int(tone))*oneToneInDec) / oneBaseInDec
+
+			break
+
+		} else if longitude >= 0 && longitude < 3.875 {
+			// здесь будут находиться все значения в градусах от 0 до 3.875 (не включительно) [25, [358.25, 3.875]]
+			// так как круг в 360 градусов завершается и начинается новый
+
+			hex = 25
+
+			number_of_passed_degrees = longitude - 358.25 + 360
+
+			line = number_of_passed_degrees / oneLineInDec
+			// console.log(`Линия  = ${math.ceil(line)}, линия завершена на ${(line - math.trunc(line))*100}  процентов`);
+
+			color = (number_of_passed_degrees - float64(int(line))*oneLineInDec) / oneColorInDec
+			// console.log(`Цвет   = ${math.ceil(color)}, цвет завершена на ${(color - math.trunc(color))*100}  процентов`);
+
+			tone = (number_of_passed_degrees - float64(int(line))*oneLineInDec - float64(int(color))*oneColorInDec) / oneToneInDec
+			// console.log(`Тон  =  ${math.ceil(tone)},тон завершен на ${(tone - math.trunc(tone))*100} процентов`);
+
+			base = (number_of_passed_degrees - float64(int(line))*oneLineInDec - float64(int(color))*oneColorInDec - float64(int(tone))*oneToneInDec) / oneBaseInDec
+
+			break
+		}
+	}
+
+	if line > 6 || color > 6 || tone > 6 || base > 5 {
+		fmt.Println("error in calc_hex_line_color_tone_base")
+	}
+
+	return structs.HdStructure{Hex: hex, Line: line, Color: color, Tone: tone, Base: base, NumberOfPassedDegrees: number_of_passed_degrees}
+}
+
+
+
+
 // TYPE
 func HdType(info *structs.CdInfo) string {
 
